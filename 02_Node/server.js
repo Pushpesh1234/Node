28c911b2a72fcb29f13daaa9432f37c6bf2c2faf
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-08-05 19:18:52 +0000

    create simple node server

---
+++ 02_Node/server.js
@@ -0,0 +1,14 @@
+// const http=require("http");
+// const server=http.createServer((req,res)=>{
+//     console.log(req)
+    
+//     process.exit()
+// })
+// console.log("hello")
+// const PORT=3000
+// server.listen(PORT,()=>{
+// console.log(`Server running on address https://verbose-waffle-wr5gjxw5v465hv6r4-${3000}.app.github.dev`)
+// })
+
+// server.listen(3000)
+ 
