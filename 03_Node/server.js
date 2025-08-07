aaabcb7296086449b2422b3d0de3f4834bb4f286
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-08-07 18:19:42 +0000

    server side rendering with differnt url

--- 03_Node/server.js
+++ 03_Node/server.js
@@ -1,14 +1,42 @@
 const http=require("http");
 const server=http.createServer((req,res)=>{
     // console.log(`URL:${req.url}, Method:${req.method}`, "Headers:", req.headers)
-    res.setHeader('Content-Type','text/html')
-    res.write(`<html><head><title></title><script src="script.js" defer></script></head> <body><h1>This is server side rendering</h1></body></html>`)
-    res.end()
+    console.log(req.url)
+    if(req.url==='/'){
+         res.setHeader('Content-Type','text/html')
+    res.write(`<html><head><title></title></head> <body><h1>Welcome to my Landing page </h1></body></html>`)
+    return res.end()
+    }
+
+
+    else if(req.url==='/products'){
+        res.setHeader('Content-Type','text/html')
+        res.write(`<html><head><title></title> </head> <body><h1>Choose Your products</h1></body></html>`)
+    return res.end()
+    }
+
+
+
+    
+    //    res.writeHead(404,"Page not found",{'Content-Type':'text/plain'})
+    //    res.end()
+
+
+
+//  res.setHeader('Content-Type','text/html')
+//     res.write(`<html><head><title></title><js" defer></script>script src="script.</head> <body><h1>Welcome to my Landing page </h1></body></html>`)
+//     res.end()
+
     // process.exit()
 })
+
+
+
 console.log("hello")
 const PORT=3000
 server.listen(PORT,()=>{
-console.log(`Server running on address https://verbose-waffle-wr5gjxw5v465hv6r4-${PORT}.app.github.dev`)
+console.log(`Server running on address https://verbose-waffle-wr5gjxw5v465hv6r4-${PORT}.app.github.dev/products`)
+// console.log(`Server running on address https://verbose-waffle-wr5gjxw5v465hv6r4-${PORT}.app.github.dev/products`)
+
 })
 
